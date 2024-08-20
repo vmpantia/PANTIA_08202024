e526b6a53f0d5c357ce4b111fab9be7aaaefa09a
Vincent Pantia <vincent.m.pantia@hotmail.com>
2024-08-20 17:40:09 +0800

    Feat: Setup docker container for the web api and database server.

---
+++ FileProcess/FileProcess.Api/Extensions/MigrationExtension.cs
@@ -0,0 +1,15 @@
+﻿using FileProcess.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileProcess.Api.Extensions
+{
+    public static class MigrationExtension
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using FileProcessDbContext context = scope.ServiceProvider.GetRequiredService<FileProcessDbContext>();
+            context.Database.Migrate();
+        }
+    }
+}
